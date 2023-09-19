3f8269f1b89136919d021c8ccd153b549606db92
Buildertoo <102919008+Buildertoo@users.noreply.github.com>
2023-09-19 15:49:18 -0230

    Added images, and edited App

--- recipe-app/src/App.js
+++ recipe-app/src/App.js
@@ -1,15 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
-import { recipeList } from './recipe';
+import { useState, useEffect } from 'react';
+import Recipe from './recipe';
 
 function App() {
-  const [recipes, setRecipes] = useState([]);
+
+  const [recipes, setRecipes] = useState(null);
+
+  useEffect( () => {
+    fetch("./recipes.json")
+    .then( response => response.json())
+    .then( setRecipes )
+    .catch( e => console.log(e.message))
+  }, [] )
+
+  if (recipes == null) return <div>Loading..</div>;
 
   return (
     <div className="App">
       <header className="App-header">
-        {recipes.map((recipe) => <Recipe key={recipe.id} {...recipe} />)} 
+        {recipes.map((recipe, index) => <Recipe key={index} recipe={recipe} />)} 
       </header>
     </div>
   );
--- recipe-app/src/recipe.js
+++ recipe-app/src/recipe.js
@@ -1,14 +1,20 @@
 export function recipeList( {recipes} ) {
     return (
         recipes.map((recipe, i) => {
-            return <Recipe key={i} name={recipe.name} />
+            return <Recipe key={i} recipe={recipe} />
         })
     )
 }
 
-function Recipe( {name} ) {
+function Recipe( {recipe} ) {
     return (
-        <p>{name}</p>
+        <div>
+            <h2>{recipe.name}</h2>
+            <img src={recipe.img} alt={recipe.name} />
+            <p><strong>Ingredients:</strong> {recipe.ingdts}</p>
+            <p><strong>Directions:</strong> {recipe.directs}</p>
+            <p><strong>Description:</strong> {recipe.desc}</p>
+        </div>
     )
 }
 
