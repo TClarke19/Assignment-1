9d0de85e8a369034f668f3b2b04fa6efb036f943
Buildertoo <102919008+Buildertoo@users.noreply.github.com>
2023-09-20 18:48:52 -0230

    Added router and edited image render scales

--- recipe-app/src/recipe.js
+++ recipe-app/src/recipe.js
@@ -10,7 +10,7 @@ function Recipe( {recipe} ) {
     return (
         <div>
             <h2>{recipe.name}</h2>
-            <img src={recipe.img} alt={recipe.name} />
+            <img style={{ width: '400px' }}src={recipe.img} alt={recipe.name} />
             <p><strong>Ingredients:</strong> {recipe.ingdts}</p>
             <p><strong>Directions:</strong> {recipe.directs}</p>
             <p><strong>Description:</strong> {recipe.desc}</p>
