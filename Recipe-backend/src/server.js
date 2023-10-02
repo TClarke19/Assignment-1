418d419ab1e8c28da7d6412ece28bdd7a61de3c6
Buildertoo <102919008+Buildertoo@users.noreply.github.com>
2023-10-02 17:49:09 -0230

    Added some params to server

--- Recipe-backend/src/server.js
+++ Recipe-backend/src/server.js
@@ -1,7 +1,19 @@
 import express from 'express';
 
 const app = express();
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Test Response');
-})
+const PORT = 8000;
+
+app.get('/api/home', async (req, res) => {
+    console.log(req.body);
+    res.send(`Test Response. ${req.body.name}`);
+});
+
+app.get('/api/form', (req, res) => {
+    res.send(``)
+});
+
+app.listen(8000, () => {
+    console.log(`Server is listening on port ${PORT}`)
+});
